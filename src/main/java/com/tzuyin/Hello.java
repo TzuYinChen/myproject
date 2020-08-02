957d77f7be91f01ac15cc49d6b079403dcefa586
tzuyinchen <j40538@gmail.com>
2020-08-02 20:01:19 +0800

    Java and Kotlin variable

--- src/main/java/com/tzuyin/Hello.java
+++ src/main/java/com/tzuyin/Hello.java
@@ -5,5 +5,11 @@ public class Hello {
 //        System.out.println("Hi~~~~~");
         Person p=new Person();
         p.hello();
+        int a=5;
+        char c='c';
+        double x=0.2;
+        Integer a1=5;
+        Character c1='c';
+        String name="Tom";
     }
 }
--- src/main/kotlin/com/tzuyin/Hello.kt
+++ src/main/kotlin/com/tzuyin/Hello.kt
@@ -5,10 +5,31 @@ fun main() {
 //    Human().hello()
     var k=Human()
     k.hello()
+    var a=10
+    var b=99.5f
+    var c="asdd12"
+    var d='a'
+
+    var a1:Int=10
+    var a2:Int
+    a2=99
+    var b1=33.4f
+    var b2:Float=33.4f
+    var b3:Float
+    b3=66.7f
+    var c1="sddf"
+    var c2:String="edfdfdgdff"
+    var c3:String
+    c3="edf"
+    var d1='a'
+    var d2:Char='b'
+    var d3:Char
+    d3='w';
+
 }
 
 class Human {
     fun hello() {
-        println("H~~~~~~")
+        println("T~~~~~~")
     }
 }
